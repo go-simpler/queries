5fb581f2ce7f38ab2f6b1e60a2a009aac26f07e9
Tom <73077675+tmzane@users.noreply.github.com>
2024-05-14 23:41:26 +0300

    feat: implement DebugString()

--- builder_test.go
+++ builder_test.go
@@ -1,6 +1,7 @@
 package queries_test
 
 import (
+	"context"
 	"testing"
 
 	"go-simpler.org/queries"
@@ -25,27 +26,32 @@ func TestBuilder_placeholders(t *testing.T) {
 	tests := map[string]struct {
 		format string
 		query  string
+		debug  string
 	}{
 		"?": {
 			format: "select * from tbl where foo = %? and bar = %? and baz = %?",
 			query:  "select * from tbl where foo = ? and bar = ? and baz = ?",
+			debug:  "select * from tbl where foo = 42 and bar = 'test' and baz = 'context.Background'",
 		},
 		"$": {
 			format: "select * from tbl where foo = %$ and bar = %$ and baz = %$",
 			query:  "select * from tbl where foo = $1 and bar = $2 and baz = $3",
+			debug:  "select * from tbl where foo = 42 and bar = 'test' and baz = 'context.Background'",
 		},
 		"@": {
 			format: "select * from tbl where foo = %@ and bar = %@ and baz = %@",
 			query:  "select * from tbl where foo = @p1 and bar = @p2 and baz = @p3",
+			debug:  "select * from tbl where foo = 42 and bar = 'test' and baz = 'context.Background'",
 		},
 	}
 
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
 			var qb queries.Builder
-			qb.Appendf(tt.format, 1, 2, 3)
+			qb.Appendf(tt.format, 42, "test", context.Background())
 			assert.Equal[E](t, qb.String(), tt.query)
-			assert.Equal[E](t, qb.Args, []any{1, 2, 3})
+			assert.Equal[E](t, qb.Args, []any{42, "test", context.Background()})
+			assert.Equal[E](t, qb.DebugString(), tt.debug)
 		})
 	}
 }
@@ -59,25 +65,25 @@ func TestBuilder_badQuery(t *testing.T) {
 			appends: func(qb *queries.Builder) {
 				qb.Appendf("select %d from tbl", "foo")
 			},
-			panicMsg: "queries.Builder: bad query: select %!d(string=foo) from tbl",
+			panicMsg: "queries: bad query: select %!d(string=foo) from tbl",
 		},
 		"too few arguments": {
 			appends: func(qb *queries.Builder) {
 				qb.Appendf("select %s from tbl")
 			},
-			panicMsg: "queries.Builder: bad query: select %!s(MISSING) from tbl",
+			panicMsg: "queries: bad query: select %!s(MISSING) from tbl",
 		},
 		"too many arguments": {
 			appends: func(qb *queries.Builder) {
 				qb.Appendf("select %s from tbl", "foo", "bar")
 			},
-			panicMsg: "queries.Builder: bad query: select foo from tbl%!(EXTRA queries.argument=bar)",
+			panicMsg: "queries: bad query: select foo from tbl%!(EXTRA queries.argument=bar)",
 		},
 		"different placeholders": {
 			appends: func(qb *queries.Builder) {
 				qb.Appendf("select * from tbl where foo = %? and bar = %$ and baz = %@", 1, 2, 3)
 			},
-			panicMsg: "queries.Builder: bad query: $?@ placeholders used",
+			panicMsg: "queries: bad query: different placeholders used",
 		},
 	}
 
